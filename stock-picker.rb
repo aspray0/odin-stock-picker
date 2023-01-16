b3892079d9d08744292301b2a792caae3f8d6f68
aspray0 <andrew.d.spray@gmail.com>
2023-01-16 11:25:03 -0800

    First draft pseudocode

---
+++ stock-picker.rb
@@ -0,0 +1,10 @@
+# stock-picker.rb
+
+# def method stock_picker that takes an array of stock prices
+
+# init buy_array w/ Array.pop
+# init sell_array w/ Array.shift
+# sub buy_array from sell_array & store in new array
+# store buy_date & sell_date as an array in a hash, with the profit as the key to the value
+# check profit_array for max_profit
+# use max_profit as key in hash to return the array of dates
