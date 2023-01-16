853ad1171d82e886abcec5987e5affb96e3fca2e
aspray0 <andrew.d.spray@gmail.com>
2023-01-16 11:34:57 -0800

    Reword primary pseudocode, fill in minor pseudocode

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,10 +1,20 @@
 # stock-picker.rb
 
 # def method stock_picker that takes an array of stock prices
+def stock_picker(stock_prices)
 
 # init buy_array w/ Array.pop
+buy_array = stock_prices.pop
+
 # init sell_array w/ Array.shift
-# sub buy_array from sell_array & store in new array
-# store buy_date & sell_date as an array in a hash, with the profit as the key to the value
+sell_array = stock_prices.shift
+
+# "reduce" sell_array into a hash, with a hash and an array of profits
+# the nested hash will have the profits as keys and an array of the buy and sell dates as values
+
 # check profit_array for max_profit
+
+
 # use max_profit as key in hash to return the array of dates
+
+end
