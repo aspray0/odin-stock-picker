5cafb1dbfdfa52c4f3881b74def24e3134ddd3bb
aspray0 <andrew.d.spray@gmail.com>
2023-01-20 11:42:54 -0800

    Adjust indexing of enumerables, clean up code

--- stock-picker.rb
+++ stock-picker.rb
@@ -3,18 +3,19 @@
 # def method stock_picker that takes an array of stock prices
 def stock_picker(stock_prices)
 
-# init buy_array w/ Array.pop
-buy_array = stock_prices.pop
+# init buy_prices & sell_prices arrays, & profit_hash
+  buy_prices = stock_prices[..(stock_prices.length - 2)]
+  sell_prices = stock_prices[1..]
+  profit_hash = {}
 
-# init sell_array w/ Array.shift
-sell_array = stock_prices.shift
-
-# "reduce" sell_array into a hash, with a hash and an array of profits
-# the nested hash will have the profits as keys and an array of the buy and sell dates as values
-
-# check profit_array for max_profit
-
-
-# use max_profit as key in hash to return the array of dates
-
+  # have to buy first, so iterate from buy_prices
+  buy_prices.each_with_index do |buy_price, buy_date|
+    # sell_prices only need to iterate from the days after the buy_date
+    sell_prices[(buy_date)..].each do |sell_price|
+      # use an array of the buy/sell dates as the key to the profit value, store in profit_hash
+      profit_hash[[buy_date, stock_prices.index(sell_price)]] = sell_price - buy_price
+    end
+  end
+  # from the profit_hash, return the buy & sell dates (key) that generates the most profit (max value)
+  return profit_hash.key(profit_hash.values.max)
 end
